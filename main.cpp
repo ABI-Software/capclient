ad967e289dc2cf483f9ef0bd8dfe8a3fb88ccd0d
Jae Do Chung <jaedo.chung@auckland.ac.nz>
2009-02-13 00:18:30 +1300

    transform the heart model to world coordinate

--- main.cpp
+++ main.cpp
@@ -289,7 +289,6 @@ void loadImagePlane(const string& name, Cmiss_command_data* command_data)
 		cout << "Can't find scene" << endl;
 	}
 
-	GT_element_settings* settings;
 	Cmiss_region* root_region = Cmiss_command_data_get_root_region(command_data);
 	//Got to find the child region first!!
 	if(!Cmiss_region_get_region_from_path(root_region, name.c_str(), &region))
@@ -298,7 +297,7 @@ void loadImagePlane(const string& name, Cmiss_command_data* command_data)
 		std::cout << "Cmiss_region_get_region_from_path() returned 0 : "<< region <<endl;
 	}
 
-	settings = CREATE(GT_element_settings)(GT_ELEMENT_SETTINGS_SURFACES);
+	GT_element_settings* settings = CREATE(GT_element_settings)(GT_ELEMENT_SETTINGS_SURFACES);
 	//hack
 	GT_element_settings_set_selected_material(settings, material);
 
@@ -439,6 +438,75 @@ int main(int argc,char *argv[])
 			{
 				std::cout << "Error reading ex file: " << string(filename) << std::endl;
 			}
+		}
+
+		//Transform the heart model to world coordinate
+
+		char* scene_object_name = "heart";
+		gtMatrix transformation_matrix, temp;
+		// ii ji ki  0
+		// ij jj kj  0    *  translation
+		// ik jk kk  0
+		//  0  0  0  1
+		Point3D i(1,0,0), j(0,1,0), k(0,0,1);
+		Point3D i_hat(0.614465, -0.714418, -0.334724), j_hat( -0.442694, -0.663401, 0.603259),
+			k_hat(-0.653035, -0.222501, -0.723905);
+		Point3D translation(42.3143, -38.2987, 11.0703);
+
+		temp[0][0] = DOT(i,i_hat);
+		temp[0][1] = DOT(j,i_hat);
+		temp[0][2] = DOT(k,i_hat);
+
+		temp[1][0] = DOT(i,j_hat);
+		temp[1][1] = DOT(j,j_hat);
+		temp[1][2] = DOT(k,j_hat);
+
+		temp[2][0] = DOT(i,k_hat);
+		temp[2][1] = DOT(j,k_hat);
+		temp[2][2] = DOT(k,k_hat);
+
+		temp[3][0] = translation.x;
+		temp[3][1] = translation.y;
+		temp[3][2] = translation.z;
+
+
+
+		transformation_matrix[0][0]=temp[0][0];
+		transformation_matrix[0][1]=temp[0][1];
+		transformation_matrix[0][2]=temp[0][2];
+		transformation_matrix[0][3]=0;
+		transformation_matrix[1][0]=temp[1][0];
+		transformation_matrix[1][1]=temp[1][1];
+		transformation_matrix[1][2]=temp[1][2];
+		transformation_matrix[1][3]=0;
+		transformation_matrix[2][0]=temp[2][0];
+		transformation_matrix[2][1]=temp[2][1];
+		transformation_matrix[2][2]=temp[2][2];
+		transformation_matrix[2][3]=0;
+		transformation_matrix[3][0]=temp[3][0];
+		transformation_matrix[3][1]=temp[3][1];
+		transformation_matrix[3][2]=temp[3][2];
+		transformation_matrix[3][3]=1;
+
+		if (scene_object_name)
+		{
+			Cmiss_scene_viewer_package* scene_viewer_package = Cmiss_command_data_get_scene_viewer_package(command_data);
+			struct Scene* scene = Cmiss_scene_viewer_package_get_default_scene(scene_viewer_package);
+			if (Scene_object* scene_object=Scene_get_Scene_object_by_name(scene,
+				scene_object_name))
+			{
+				Scene_object_remove_time_dependent_transformation(scene_object);
+				Scene_object_set_transformation(scene_object,
+					&transformation_matrix);
+			}
+			else
+			{
+				display_message(ERROR_MESSAGE,"No object named '%s' in scene",scene_object_name);
+			}
+		}
+		else
+		{
+			display_message(ERROR_MESSAGE,"Missing graphics object name");
 		}
 
 		loadImagePlane("SA1", command_data);
