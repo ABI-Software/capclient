c06616d32df6f0aec127a3eea61d62608a009929
Jae Do Chung <jaedo.chung@auckland.ac.nz>
2009-05-27 18:56:09 +1200

    Moved state/mode dependent code from CAPModeller to CAPModellingModeGuidePoints - WIP fiducial mark placement (Bus error putting guide point)

--- src/CAPModellingMode.h
+++ src/CAPModellingMode.h
@@ -90,17 +90,56 @@ private:
 	std::vector<DataPoint> BasePlanePoints_; // holds n pairs of DataPoints ( n >= 1 )
 };
 
+#include "CAPTimeSmoother.h"
+
+class CAPModelLVPS4X4;
+class Matrix;
+class Vector;
+class Preconditioner;
+class GSmoothAMatrix;
+class SolverLibraryFactory;
+
 class CAPModellingModeGuidePoints : public CAPModellingMode
 {
 public:
-	CAPModellingModeGuidePoints(CAPModeller& modeller)
-	: CAPModellingMode(modeller)
-	{}
+	typedef std::map<Cmiss_node*, DataPoint> DataPoints;
+	
+	CAPModellingModeGuidePoints(CAPModeller& modeller, CAPModelLVPS4X4 heartModel);
+	~CAPModellingModeGuidePoints();
 	
 	CAPModellingMode* OnAccept();
 	void AddDataPoint(Cmiss_node* dataPointID, const DataPoint& dataPoint);
 	void MoveDataPoint(Cmiss_node* dataPointID, const Point3D& coord, float time);
 	void RemoveDataPoint(Cmiss_node* dataPointID, float time);
+	
+	void InitialiseModel();
+	void ReadModelFromFile(std::string& filename);
+	void UpdateTimeVaryingDataPoints(const Vector& x, int frameNumber);
+	void UpdateTimeVaryingModel();
+	void SmoothAlongTime();
+	
+private:
+	void FitModel(DataPoints& dataPoints, int frameNumber);
+	
+	std::vector<float> ConvertToHermite(const Vector&);
+	
+	CAPModelLVPS4X4& heartModel_;
+	
+	std::vector<DataPoints> vectorOfDataPoints_;
+	
+	std::vector< std::vector<float> > timeVaryingDataPoints_;
+	
+	SolverLibraryFactory* solverFactory_;
+	Matrix* S_;
+	Matrix* G_;
+	Matrix* P_;
+	Preconditioner* preconditioner_;
+	GSmoothAMatrix* aMatrix_;
+	Vector* prior_;
+	
+	Matrix* bezierToHermiteTransform_; // Temporary
+	
+	CAPTimeSmoother timeSmoother_;
 };
 
 #endif /* CAPMODELLINGMODE_H_ */
