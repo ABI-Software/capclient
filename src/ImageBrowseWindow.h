68cb2228ccad783db27d714cbe70fc29753386e9
Jae Do Chung <jaedo.chung@auckland.ac.nz>
2010-06-22 02:52:29 +1200

    Initial implementation of image preview - stil quite buggy

--- src/ImageBrowseWindow.h
+++ src/ImageBrowseWindow.h
@@ -13,14 +13,24 @@
     #include "wx/wx.h"
 #endif
 
+#include <boost/tr1/memory.hpp>
 #include <string>
+#include <vector>
+#include <map>
 
 struct Cmiss_scene_viewer;
 typedef struct Cmiss_scene_viewer *Cmiss_scene_viewer_id;
+struct Cmiss_texture;
+typedef struct Cmiss_texture* Cmiss_texture_id;
+struct Graphical_material;
+
 class wxListCtrl;
+class wxListEvent;
 
 namespace cap
 {
+
+class DICOMImage;
 
 class ImageBrowseWindow : public wxFrame
 {
@@ -29,13 +39,30 @@ public:
 	virtual ~ImageBrowseWindow();
 	
 private:
+	typedef std::pair<int, double> SliceKeyType;
+	typedef std::tr1::shared_ptr<DICOMImage> DICOMPtr;
+	typedef std::map<SliceKeyType, std::vector<DICOMPtr> > SliceMap;
+	typedef std::map<SliceKeyType, std::vector<Cmiss_texture_id> > TextureMap;
+		
 	wxString GetCellContentsString( long row_number, int column );
 	void PopulateImageTable();
+	void LoadImages();
+	void SwitchSliceToDisplay(SliceKeyType const& key);
+	void LoadImagePlaneModel();
+	void DisplayImage(Cmiss_texture_id tex);
 	
 	std::string archiveFilename_;
 	Cmiss_scene_viewer_id sceneViewer_;
+	Graphical_material* material_;
+	
 	wxListCtrl* imageTable_;
+	
+	SliceMap sliceMap_;
+	TextureMap textureMap_; // this could be merged with sliceMap_
+	
 	//Event handlers
+	void OnImageTableItemSelected(wxListEvent& event);
+	
 	DECLARE_EVENT_TABLE();
 };
 
