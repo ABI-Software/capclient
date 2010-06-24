f31dbc8e4799b7cba2240e1cc6303dbf6f1f70e7
Jae Do Chung <jaedo.chung@auckland.ac.nz>
2010-06-24 18:42:00 +1200

    Moved implementation of CAPMaterial from the header file to cpp file. Now texture is properly registered to the texture managere on creation.

--- src/CAPMaterial.h
+++ src/CAPMaterial.h
@@ -12,21 +12,13 @@
 
 #include <boost/noncopyable.hpp>
 #include <string>
-#include <sstream>
-#include <fstream>
-#include <iostream>
-
-extern "C"
-{
-#include "graphics/material.h"
-}
 
 struct Cmiss_texture;
 typedef Cmiss_texture* Cmiss_texture_id;
 struct Graphical_material;
 typedef Graphical_material* Cmiss_material_id;;
-
-int Graphical_material_changed(struct Graphical_material *material);
+struct Cmiss_graphics_module;
+typedef Cmiss_graphics_module* Cmiss_graphics_module_id;
 
 namespace cap
 {
@@ -37,61 +29,7 @@ namespace cap
 class CAPMaterial : public boost::noncopyable
 {
 public:
-	CAPMaterial(std::string const& materialName, Cmiss_graphics_module_id graphics_module)
-	:
-	regionName_(materialName),
-	material_(0),
-	brightnessAndContrastTexture_(0)
-	{
-		if (materialName.empty())
-		{
-			return;
-		}
-		
-		// User has to make sure each material has correct name as it is also used 
-		// for the region to which this material is applied.
-		material_ = Cmiss_graphics_module_create_material(graphics_module);
-
-		// Initialize shaders that are used for adjusting brightness and contrast
-		using namespace std;
-		
-		stringstream vp_stream, fp_stream;
-		ifstream is;
-		is.open("Data/shaders/vp.txt");
-		vp_stream << is.rdbuf();
-		is.close();
-		
-		is.open("Data/shaders/fp.txt");
-		fp_stream << is.rdbuf();
-		is.close();
-		
-		if (!Material_set_material_program_strings(material_, 
-				(char*) vp_stream.str().c_str(), (char*) fp_stream.str().c_str())
-				)
-		{
-			cout << "Error: cant set material program strings" << endl;
-		}
-		
-		// Initialize the texture used to pass brightness and contrast to fragment shader
-		string brightnessAndContrastTextureName(materialName + "_BrightnessAndContrast"); 
-		brightnessAndContrastTexture_ = CREATE(Texture)(brightnessAndContrastTextureName.c_str());
-		unsigned char source_pixels[4] = {128,128,0,255};//TEST
-		if (!Cmiss_texture_set_pixels(brightnessAndContrastTexture_,
-			1 /*int width */, 1/*int height*/, 1/*int depth*/,
-			4 /*int number_of_components*/, 1 /*int number_of_bytes_per_component*/,
-			4 /*int source_width_bytes*/, source_pixels))
-		{
-			//Error
-			cout << "ImageSlice::ImageSlice() Error setting pixel value to brightnessAndContrastTexture_" << endl;
-		}
-		
-		// Note: cmgui provided an api function Cmiss_material_set_texture
-		// which calls Graphical_material_changed()
-		// but no equivalent exists for Graphical_material_set_second_texture
-		// which here is used for the procedural texture that is used to pass on the
-		// brightness and contrast value to the OpenGL shaders.
-		Graphical_material_set_second_texture(material_, brightnessAndContrastTexture_);
-	}
+	CAPMaterial(std::string const& materialName, Cmiss_graphics_module_id graphics_module);
 	
 	~CAPMaterial()
 	{
@@ -100,73 +38,11 @@ public:
 //		DESTROY(Texture)(&brightnessAndContrastTexture_);
 	}
 	
-	void SetBrightness(float brightness)
-	{
-		unsigned char pixels[4];
-		unsigned char fill[1] = {0};
-		Cmiss_texture_get_pixels(brightnessAndContrastTexture_,
-			0, 0, 0,
-			1, 1, 1,
-			0, 0, 
-			fill,
-			4, pixels);
-		
-		pixels[0] = 255.0 * brightness;
-		
-		if (!Cmiss_texture_set_pixels(brightnessAndContrastTexture_,
-			1 /*int width */, 1/*int height*/, 1/*int depth*/,
-			4 /*int number_of_components*/, 1 /*int number_of_bytes_per_component*/,
-			4 /*int source_width_bytes*/, pixels))
-		{
-			//Error
-			std::cout << "ImageSlice::SetBrightness() Error setting pixel value to brightnessAndContrastTexture_\n";
-		}
-		Texture_notify_change(brightnessAndContrastTexture_);
-	}
+	void SetBrightness(float brightness);
 	
-	void SetContrast(float contrast)
-	{
-		unsigned char pixels[4];
-		unsigned char fill[1] = {0};
-		Cmiss_texture_get_pixels(brightnessAndContrastTexture_,
-			0, 0, 0,
-			1, 1, 1,
-			0, 0, 
-			fill,
-			4, pixels);
-		
-		pixels[1] = 255.0 * contrast;
-		
-		if (!Cmiss_texture_set_pixels(brightnessAndContrastTexture_,
-			1 /*int width */, 1/*int height*/, 1/*int depth*/,
-			4 /*int number_of_components*/, 1 /*int number_of_bytes_per_component*/,
-			4 /*int source_width_bytes*/, pixels))
-		{
-			//Error
-			std::cout << "ImageSlice::SetContrast() Error setting pixel value to brightnessAndContrastTexture_\n";
-		}
-		Texture_notify_change(brightnessAndContrastTexture_);
-		Graphical_material_changed(material_);
-	}
+	void SetContrast(float contrast);
 	
-	void ChangeTexture(Cmiss_texture_id tex)
-	{	
-		if (material_)
-		{
-			// Cmiss_material_set_texture calls Graphical_material_changed(material)
-			// Whereas Graphical_material_set_texture doesn't
-			// Currently no such function exists for Graphical_material_set_second_texture
-			if (!Cmiss_material_set_texture(material_,tex))
-			{
-				//Error
-				std::cout << "Error: Cmiss_material_set_texture()\n";
-			}
-		}
-		else
-		{
-			std::cout << __func__ << " - Error: null material pointer\n";
-		}
-	}
+	void ChangeTexture(Cmiss_texture_id tex);
 	
 	Cmiss_material_id GetCmissMaterial() const
 	{
@@ -174,7 +50,7 @@ public:
 	}
 	
 private:
-	std::string regionName_;
+//	std::string materialName_;
 	Cmiss_material_id material_;
 	Cmiss_texture_id brightnessAndContrastTexture_;
 };
