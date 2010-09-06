9b27fb7241673b400737ffb8712f853141ed924d
Jae Do Chung <jaedo.chung@auckland.ac.nz>
2010-09-06 16:46:06 +1200

    Added the feature to show a pop up dialog for EULA See http://sourceforge.net/tracker/?func=detail&aid=2832924&group_id=237340&atid=1103261

--- src/CAPHtmlWindow.h
+++ src/CAPHtmlWindow.h
@@ -13,7 +13,6 @@
 namespace cap
 {
 
-
 class CAPHtmlWindow: public wxHtmlWindow
 {
 public:
@@ -22,23 +21,6 @@ public:
 		long style = wxHW_SCROLLBAR_AUTO, const wxString& name = _T("htmlWindow"));
 	void OnLinkClicked(const wxHtmlLinkInfo& link);
 };
- 
-CAPHtmlWindow::CAPHtmlWindow(wxWindow *parent, wxWindowID id, const wxPoint& pos,
-	const wxSize& size, long style, const wxString& name)
-: wxHtmlWindow(parent, id, pos, size, style, name)
-{
-	// we use PNG & JPEG images in our HTML page
-	//wxImage::AddHandler(new wxJPEGHandler);
-	wxImage::AddHandler(new wxPNGHandler);
-}
- 
-void CAPHtmlWindow::OnLinkClicked(const wxHtmlLinkInfo& link)
-{
-	if (link.GetHref().StartsWith(_T("http://")))
-		wxLaunchDefaultBrowser(link.GetHref());
-	else
-		wxHtmlWindow::OnLinkClicked(link);
-}
 
 } // end namespace cap
 #endif /* CAPHTMLWINDOW_H_ */
