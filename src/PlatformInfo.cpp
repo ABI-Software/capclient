cd26804d053ed3a14ce9ee219b5f1e267f99b655
jchu014 <jchu014@bioeng1167.localdomain>
2010-09-08 16:34:03 +1200

    On Windows, now the os version is obtained by looking at the registry rather than using WMI since MinGW can't compile WMI code.

--- src/PlatformInfo.cpp
+++ src/PlatformInfo.cpp
@@ -66,178 +66,25 @@ namespace cap
 
 std::string PlatformInfo::GetOSVersion()
 {
-    HRESULT hres;
-    std::string version;
+	HKEY hKey; // Declare a key to store the result
+	DWORD buffersize = 1024; // Declare the size of the data buffer
+	char* lpData = new char[buffersize];// Declare the buffer
 
-    // Step 1: --------------------------------------------------
-    // Initialize COM. ------------------------------------------
-    hres =  CoInitializeEx(0, COINIT_MULTITHREADED); 
-    if (FAILED(hres))
-    {
-        cout << "Failed to initialize COM library. Error code = 0x" 
-            << hex << hres << endl;
-        return 1;                  // Program has failed.
-    }
-
-    // Step 2: --------------------------------------------------
-    // Set general COM security levels --------------------------
-    // Note: If you are using Windows 2000, you need to specify -
-    // the default authentication credentials for a user by using
-    // a SOLE_AUTHENTICATION_LIST structure in the pAuthList ----
-    // parameter of CoInitializeSecurity ------------------------
-    hres =  CoInitializeSecurity(
-        NULL, 
-        -1,                          // COM authentication
-        NULL,                        // Authentication services
-        NULL,                        // Reserved
-        RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
-        RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
-        NULL,                        // Authentication info
-        EOAC_NONE,                   // Additional capabilities 
-        NULL                         // Reserved
-        );
-                      
-    if (FAILED(hres))
-    {
-        cout << "Failed to initialize security. Error code = 0x" 
-            << hex << hres << endl;
-        CoUninitialize();
-        return std::string();                    // Program has failed.
-    }
-    
-    // Step 3: ---------------------------------------------------
-    // Obtain the initial locator to WMI -------------------------
-    IWbemLocator *pLoc = NULL;
-
-    hres = CoCreateInstance(
-        CLSID_WbemLocator,             
-        0, 
-        CLSCTX_INPROC_SERVER, 
-        IID_IWbemLocator, (LPVOID *) &pLoc);
- 
-    if (FAILED(hres))
-    {
-        cout << "Failed to create IWbemLocator object."
-            << " Err code = 0x"
-            << hex << hres << endl;
-        CoUninitialize();
-        return std::string();                 // Program has failed.
-    }
-
-    // Step 4: -----------------------------------------------------
-    // Connect to WMI through the IWbemLocator::ConnectServer method
-
-    IWbemServices *pSvc = NULL;
+	RegOpenKeyEx (HKEY_LOCAL_MACHINE,
+	"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion",NULL,KEY_READ,&hKey);
 	
-    // Connect to the root\cimv2 namespace with
-    // the current user and obtain pointer pSvc
-    // to make IWbemServices calls.
-    hres = pLoc->ConnectServer(
-         _bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
-         NULL,                    // User name. NULL = current user
-         NULL,                    // User password. NULL = current
-         0,                       // Locale. NULL indicates current
-         NULL,                    // Security flags.
-         0,                       // Authority (e.g. Kerberos)
-         0,                       // Context object 
-         &pSvc                    // pointer to IWbemServices proxy
-         );
-    
-    if (FAILED(hres))
-    {
-        cout << "Could not connect. Error code = 0x" 
-             << hex << hres << endl;
-        pLoc->Release();     
-        CoUninitialize();
-        return std::string();                // Program has failed.
-    }
-//    cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
-
-    // Step 5: --------------------------------------------------
-    // Set security levels on the proxy -------------------------
-
-    hres = CoSetProxyBlanket(
-       pSvc,                        // Indicates the proxy to set
-       RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
-       RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
-       NULL,                        // Server principal name 
-       RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
-       RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
-       NULL,                        // client identity
-       EOAC_NONE                    // proxy capabilities 
-    );
-
-    if (FAILED(hres))
-    {
-        std::cout << "Could not set proxy blanket. Error code = 0x" 
-            << hex << hres << endl;
-        pSvc->Release();
-        pLoc->Release();     
-        CoUninitialize();
-        return std::string();               // Program has failed.
-    }
-
-    // Step 6: --------------------------------------------------
-    // Use the IWbemServices pointer to make requests of WMI ----
-
-    // For example, get the name of the operating system
-    IEnumWbemClassObject* pEnumerator = NULL;
-    hres = pSvc->ExecQuery(
-        bstr_t("WQL"), 
-        bstr_t("SELECT * FROM Win32_OperatingSystem"),
-        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY, 
-        NULL,
-        &pEnumerator);
-    
-    if (FAILED(hres))
-    {
-        std::cout << "Query for operating system name failed."
-            << " Error code = 0x" 
-            << hex << hres << endl;
-        pSvc->Release();
-        pLoc->Release();
-        CoUninitialize();
-        return std::string();               // Program has failed.
-    }
-
-    // Step 7: -------------------------------------------------
-    // Get the data from the query in step 6 -------------------
- 
-    IWbemClassObject *pclsObj;
-    ULONG uReturn = 0;
-   
-    while (pEnumerator)
-    {
-        HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, 
-            &pclsObj, &uReturn);
-
-        if(0 == uReturn)
-        {
-            break;
-        }
-
-        VARIANT vtProp;
-
-        // Get the value of the Name property
-        hr = pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
-        
-//        wcout << " OS Name : " << vtProp.bstrVal << endl;
-        
-        wstring wstr(vtProp.bstrVal);
-        version.assign(wstr.begin(), wstr.end());
-        
-        VariantClear(&vtProp);
-        pclsObj->Release();
-    }
-
-    // Cleanup
-    // ========
-    
-    pSvc->Release();
-    pLoc->Release();
-    pEnumerator->Release();
-    CoUninitialize();
-
+	// Query the registry value
+	RegQueryValueEx(hKey,"ProductName",NULL,NULL,(LPBYTE) lpData,&buffersize);
+	
+	// Print out the registry value
+//	std::cout << "Registry Key Open: memory location=" << hKey << "\n";
+//	std::cout << "Your Operating System is " << lpData << "\n\n";
+	std::string version(lpData);
+	
+	// Close the Registry Key
+	RegCloseKey (hKey);
+	delete lpData;
+	
     return version;   // Program successfully completed.
 }
 
