27afec3a43b3162ee1e9e0fc5c95b8f8321f05f7
Jae Do Chung <jaedo.chung@auckland.ac.nz>
2010-10-26 20:28:31 +1300

    Adding contour support (wip) contours are now correctly displayed according to their visibility field

--- src/CAPContour.cpp
+++ src/CAPContour.cpp
@@ -12,8 +12,10 @@
 
 extern "C" {
 #include "api/cmiss_region.h"
+#include "finite_element/finite_element_region.h"
 }
-#include "CmguiExtensions.h"
+//#include "CmguiExtensions.h"
+#include "computed_field/computed_field_finite_element.h"
 
 namespace cap
 {
@@ -37,7 +39,9 @@ namespace
 
 int AddNodesToVector(Cmiss_node_id node, void *user_data)
 {
-	std::vector<Cmiss_node_id> nodes = *((std::vector<Cmiss_node_id>*)(user_data));
+//	std::cout << __func__ << '\n';
+	std::vector<Cmiss_node_id>& nodes = *((std::vector<Cmiss_node_id>*)(user_data));
+	nodes.push_back(node);
 }
 
 } // unnamed namespace
@@ -59,12 +63,6 @@ void CAPContour::ReadFromExFile(std::string const& filename, Cmiss_context_id co
 	//DEBUG
 	std::cout << "contour region = " << regionName <<"\n";
 	
-	std::stringstream ss;
-	ss << "gfx mod g_e ";
-	ss << regionName;
-	ss << " node_points";
-	Cmiss_context_execute_command(context, ss.str().c_str());
-	
 	//Store pointers to nodes so we can directly manipulate their visibility attributes
 	nodes_.clear();
 	
@@ -77,20 +75,181 @@ void CAPContour::ReadFromExFile(std::string const& filename, Cmiss_context_id co
 	}
 	Cmiss_region_for_each_node_in_region(region, AddNodesToVector, (void*)&nodes_);
 	
+	// Create visibility field //TODO Factor out
+	CM_field_type cm_field_type = CM_GENERAL_FIELD;
+	char* name = (char*)"visibility";
+	Coordinate_system coordinate_system;
+	coordinate_system.type = RECTANGULAR_CARTESIAN;
+	Value_type value_type = FE_VALUE_VALUE;
+	const int number_of_components = 1;
+	char* component_names[] = {(char*)"visibility"};
+	
+	FE_region_get_FE_field_with_properties(
+		Cmiss_region_get_FE_region(region),
+		name, GENERAL_FE_FIELD,
+		/*indexer_field*/(struct FE_field *)NULL, /*number_of_indexed_values*/0,
+		cm_field_type, &coordinate_system,
+		value_type, number_of_components, component_names,
+		/*number_of_times*/0, /*time_value_type*/UNKNOWN_VALUE,
+		/*external*/(struct FE_field_external_information *)NULL);
+	
+//	manager_Computed_field* cfm = Cmiss_region_get_Computed_field_manager(region);
+//	Computed_field* field = FIND_BY_IDENTIFIER_IN_MANAGER(Computed_field, name)("visibility",cfm);
+	
+	std::stringstream ss;
+	ss << "gfx mod g_e ";
+	ss << regionName;
+	ss << " node_points visibility_field visibility";
+	
+	
+	Cmiss_context_execute_command(context, ss.str().c_str());
+	
 	// Clean up
 	Cmiss_region_destroy(&region);
 	Cmiss_region_destroy(&root_region);
 }
 
+namespace {
+
+Cmiss_node_id Cmiss_node_set_visibility_field_private(Cmiss_node_id node, 
+		struct FE_region* fe_region, 
+		Cmiss_field* visibilityField,
+		struct FE_field *fe_field, double startTime, double endTime, bool visibility)
+{
+	std::cout << __func__ << " : start = " << startTime << " , end = " << endTime << '\n';
+	
+	struct FE_node_field_creator *node_field_creator;
+	
+	if (node_field_creator = CREATE(FE_node_field_creator)(
+		/*number_of_components*/1))
+	{
+		struct FE_time_sequence *fe_time_sequence;
+		FE_value times[5];
+		double values[5];
+		int numberOfTimes;
+		double newFieldValue = visibility ? 1.0f : 0.0f;
+		
+		//Handle edge cases
+		if (startTime == 0.0f && endTime == 1.0f)
+		{
+			times[0] = 0.0f;
+			times[1] = 1.0f;
+			values[0] = newFieldValue;
+			values[1] = newFieldValue;
+			numberOfTimes = 2;
+		}
+		else if (startTime == 0.0f)
+		{
+			times[0] = 0.0f;
+			times[1] = endTime;
+			times[2] = 1.0f;
+			values[0] = newFieldValue;
+			values[1] = newFieldValue;
+			values[2] = 0;
+			numberOfTimes = 3;
+		}
+		else if (endTime == 1.0f)
+		{
+			times[0] = 0.0f;
+			times[1] = startTime;
+			times[2] = 1.0f;
+			values[0] = 0;
+			values[1] = newFieldValue;
+			values[2] = newFieldValue;
+			numberOfTimes = 3;
+		}
+		else
+		{
+			times[0] = 0.0f;
+			times[1] = startTime;
+			times[2] = endTime;
+			times[3] = 1.0f;
+			values[0] = 0;
+			values[1] = newFieldValue;
+			values[2] = newFieldValue;
+			values[3] = 0;
+			numberOfTimes = 4;
+		}
+		
+		if (!(fe_time_sequence = FE_region_get_FE_time_sequence_matching_series(
+								fe_region, numberOfTimes, times)))
+		{
+			//Error
+			std::cout << "Error: " << __func__ << " can't get time_sequence" << std::endl;
+		}
+		
+		if (define_FE_field_at_node(node,fe_field,
+			/*(struct FE_time_sequence *)NULL*/ fe_time_sequence,
+			node_field_creator))
+		{
+			int number_of_values;
+			for (int i = 0; i < numberOfTimes; ++i)
+			{									 													
+				Cmiss_field_set_values_at_node( visibilityField, node, times[i] , 1 , &(values[i]));
+			}
+			DESTROY(FE_node_field_creator)(&node_field_creator);
+			return node;
+		}
+		else
+		{
+			std::cout << "Error define_FE_field_at_node\n";
+		}
+	}
+}
+
+
+}
+
 void CAPContour::SetVisibility(bool visibility)
 {
-	std::for_each(nodes_.begin(), nodes_.end(),
-			boost::bind(Cmiss_node_set_visibility_field, _1, startTime_, endTime_, visibility)
-	);
+	std::cout << __func__ << ": " << nodes_.size() << '\n';
+	Cmiss_node_id node = nodes_.at(0);
+	
+	FE_region* fe_region = FE_node_get_FE_region(node);
+	
+	Cmiss_region* cmiss_region;
+	FE_region_get_Cmiss_region(fe_region, &cmiss_region);
+		
+//	fe_region = FE_region_get_data_FE_region(fe_region);
+//	if (!fe_region)
+//	{
+//		std::cout << "fe_region is null" << std::endl;
+//	}
+	
+	manager_Computed_field* cfm = Cmiss_region_get_Computed_field_manager(cmiss_region);
+	Computed_field* visibilityField = FIND_BY_IDENTIFIER_IN_MANAGER(Computed_field, name)("visibility",cfm);
+	
+	if (!visibilityField)
+	{
+		display_message(ERROR_MESSAGE,
+			"Cmiss_node_set_visibility_field.  Can't find visibility field");
+	}
+	
+	struct FE_field *fe_field;
+	struct LIST(FE_field) *fe_field_list;
+	if (visibilityField && (fe_field_list=
+						Computed_field_get_defining_FE_field_list(visibilityField)))
+	{
+		if ((1==NUMBER_IN_LIST(FE_field)(fe_field_list))&&
+			(fe_field=FIRST_OBJECT_IN_LIST_THAT(FE_field)(
+			(LIST_CONDITIONAL_FUNCTION(FE_field) *)NULL,(void *)NULL,
+			fe_field_list)) && (1 == get_FE_field_number_of_components(
+			fe_field)) && (FE_VALUE_VALUE == get_FE_field_value_type(fe_field)))
+		{
+//			Cmiss_node_set_visibility_field_private( node, fe_region, visibilityField, fe_field, startTime, endTime, visibility);
+			std::for_each(nodes_.begin(), nodes_.end(),
+					boost::bind(Cmiss_node_set_visibility_field_private, _1, fe_region, visibilityField,
+							fe_field, startTime_, endTime_, visibility));
+		}
+	}
+	
+//	std::for_each(nodes_.begin(), nodes_.end(),
+//			boost::bind(Cmiss_node_set_visibility_field, _1, startTime_, endTime_, visibility));
 }
 
 void CAPContour::SetValidPeriod(double startTime, double endTime)
 {
+//	std::cout << __func__ <<'\n';
 	const double EPSILON = std::numeric_limits<double>::epsilon();
 	startTime_ = startTime;
 	endTime_ = endTime - EPSILON;
