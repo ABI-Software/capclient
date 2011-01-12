0d3fe379bf3543cfd08ca1c745ffc5b5b9e3a50a
Jae Do Chung <jaedo.chung@auckland.ac.nz>
2011-01-12 22:29:04 +1100

    Checking in ImageSetBuilder.h Previous check in didnt actually include this file

---
+++ src/ImageSetBuilder.h
@@ -0,0 +1,60 @@
+/*
+ * ImageSetBuilder.h
+ *
+ *  Created on: Jan 11, 2011
+ *      Author: jchu014
+ */
+
+#ifndef IMAGESETBUILDER_H_
+#define IMAGESETBUILDER_H_
+
+#include "SliceInfo.h"
+#include "ImageSet.h"
+#include "ImageSlice.h"
+#include <boost/make_shared.hpp>
+
+namespace cap
+{
+
+class CmguiManager;
+class ImageSet;
+
+// This is the class responsible for creating the image set
+// along with the image slices that the image set owns.
+
+class ImageSetBuilder
+{
+public:
+	ImageSetBuilder(SlicesWithImages const& slices, CmguiManager const& cmguiManager)
+	:
+		slices_(slices),
+		cmguiManager_(cmguiManager)
+	{
+	}
+	
+	ImageSet* build()
+	{
+		ImageSlicesMap imageSlicesMap;
+		std::vector<std::string> imageSliceNames;
+		
+		SlicesWithImages::const_iterator itr = slices_.begin();
+		SlicesWithImages::const_iterator end = slices_.end();
+		for (;itr != end; ++itr)
+		{
+			std::string const& name(itr->GetLabel());
+			imageSlicesMap[name] = boost::make_shared<ImageSlice>(*itr, cmguiManager_);
+			imageSliceNames.push_back(name);
+		}
+		
+		return new ImageSet(slices_, imageSliceNames, imageSlicesMap);
+	}
+	
+private:
+	SlicesWithImages const& slices_;
+	CmguiManager const& cmguiManager_;
+};
+
+
+} // namespace cap
+
+#endif /* IMAGESETBUILDER_H_ */
