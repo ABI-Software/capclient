adf93f3a35a05017fecc864d3e50a3c94fdf3ace
Jae Do Chung <jaedo.chung@auckland.ac.nz>
2011-01-30 18:46:51 +1100

    Adding more tests for CAPAnnotationFile

--- test/UnitTestCAPAnnotationFile.cpp
+++ test/UnitTestCAPAnnotationFile.cpp
@@ -31,74 +31,37 @@ TEST(CAPAnnotationFile, ReadXML)
 	CAPAnnotationFile anno(test_file);
 	anno.ReadFile();
 	
-//	CAPXMLFile xmlFile(test_file);
-//
-//	xmlFile.ReadFile();
-//	EXPECT_EQ(xmlFile.chamber_, "LV");
-//	EXPECT_EQ(xmlFile.output_.focalLength, 10.0);
-//	EXPECT_EQ(xmlFile.output_.interval, 0.3);
-//	EXPECT_EQ(xmlFile.name_, "SampleAnalysisUsingXsd");
-//	EXPECT_EQ(xmlFile.studyIUid_, "2.16.124.113543.6006.99.03832048922002137666");
-//	
-//	CAPXMLFile::Image& image = xmlFile.GetInput().images.at(0);
-//	EXPECT_TRUE(image.imagePosition);
-//	EXPECT_EQ(*(image.imagePosition), Point3D(4.0, 5.0, 6.0));
-//	
-//	EXPECT_TRUE(image.imageOrientation);
-//	EXPECT_EQ(image.imageOrientation->first, Vector3D(7.0, 8.0, 9.0));
-//	EXPECT_EQ(image.imageOrientation->second, Vector3D(10.0, 11.0, 12.0));
-//
-//	EXPECT_EQ(image.contourFiles.at(0).fileName, "cap:ContourFile");
-//	EXPECT_EQ(image.contourFiles.at(0).number, 1);
-//	
-//	CAPXMLFile::Exnode& exnode = xmlFile.GetOutput().exnodes.at(0);
-//	EXPECT_EQ(exnode.exnode, "cap.exnode");
-//	EXPECT_EQ(exnode.frame, 1);
-//
-//	CAPXMLFile::ProvenanceDetail& pd = xmlFile.documentation_.provenanceDetails[0];
-//	EXPECT_EQ(pd.comment, "Converted from CIM model");
-//	// add more tests
+	EXPECT_EQ(anno.cardiacAnnotation_.imageAnnotations.size(), 234);
+	
+	CAPAnnotationFile::ImageAnnotation& imageAnnotation =
+			anno.cardiacAnnotation_.imageAnnotations.at(0);
+	
+	EXPECT_EQ(imageAnnotation.sopiuid, 
+			std::string("2.16.124.113543.6006.99.07104390983379737982"));
+	EXPECT_EQ(imageAnnotation.rOIs.at(0).labels.at(0).rid, "SNOMEDID:T-32004");
+	EXPECT_EQ(imageAnnotation.rOIs.at(0).labels.at(0).scope, "Slice");
+	EXPECT_EQ(imageAnnotation.rOIs.at(0).labels.at(0).label, "Apex of Heart");
+	EXPECT_DOUBLE_EQ(imageAnnotation.rOIs.at(0).points.at(0).x, 125.04440497335702);
+	EXPECT_DOUBLE_EQ(imageAnnotation.rOIs.at(0).points.at(0).y, 117.31438721136767);
+	EXPECT_EQ(imageAnnotation.rOIs.at(0).points.at(0).number, -1);
+	
+	EXPECT_EQ(imageAnnotation.labels.at(0).rid, "RID:10741");
+	EXPECT_EQ(imageAnnotation.labels.at(0).scope, "Series");
+	EXPECT_EQ(imageAnnotation.labels.at(0).label, "Fast Gradient Echo");
 }
 
 TEST(CAPAnnotationFile, WriteXML)
 {
-//	using namespace cap;
-//		
-//	CAPXMLFile xmlFile(test_file);
-//
-//	xmlFile.ReadFile();
-//	xmlFile.WriteFile("dummy");
-//	// add tests here
-//
-//	CAPXMLFile xmlFile2("dummy");
-//	xmlFile2.ReadFile();
-//	
-//	EXPECT_EQ(xmlFile.chamber_, xmlFile2.chamber_);
-//	EXPECT_EQ(xmlFile.output_.focalLength, xmlFile2.output_.focalLength);
-//	EXPECT_EQ(xmlFile.output_.interval, xmlFile2.output_.interval);
-//	EXPECT_EQ(xmlFile.name_, xmlFile2.name_);
-//	EXPECT_EQ(xmlFile.studyIUid_, xmlFile2.studyIUid_);
-//	
-//	CAPXMLFile::Image& image = xmlFile.GetInput().images.at(0);
-//	CAPXMLFile::Image& image2 = xmlFile2.GetInput().images.at(0);
-//	
-//	EXPECT_EQ(*(image.imagePosition), *(image2.imagePosition));
-//	
-//	EXPECT_EQ(image.imageOrientation->first, image2.imageOrientation->first);
-//	EXPECT_EQ(image.imageOrientation->second, image2.imageOrientation->second);
-//
-//	EXPECT_EQ(image.contourFiles.at(0).fileName, image2.contourFiles.at(0).fileName);
-//	EXPECT_EQ(image.contourFiles.at(0).number, image2.contourFiles.at(0).number);
-//	
-//	CAPXMLFile::Exnode& exnode = xmlFile.GetOutput().exnodes.at(0);
-//	CAPXMLFile::Exnode& exnode2 = xmlFile2.GetOutput().exnodes.at(0);
-//	EXPECT_EQ(exnode.exnode, exnode2.exnode);
-//	EXPECT_EQ(exnode.frame, exnode2.frame);
-//
-//	CAPXMLFile::ProvenanceDetail& pd = xmlFile.documentation_.provenanceDetails[0];
-//	CAPXMLFile::ProvenanceDetail& pd2 = xmlFile2.documentation_.provenanceDetails[0];
-//	EXPECT_EQ(pd.comment, pd2.comment);
-//	
+	using namespace cap;
+	
+	CAPAnnotationFile anno(test_file);
+	anno.ReadFile();
+	
+	anno.WriteFile("dummy");
+	
+//	CAPAnnotationFile anno2("dummy");
+//	anno2.ReadFile();
+	
 //	EXPECT_EQ(remove("dummy"), 0);
 }
 
