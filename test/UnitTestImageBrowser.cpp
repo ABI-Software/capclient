f2c233ff84498d408923c17191ad9d91e5ab4a82
Jae Do Chung <jaedo.chung@auckland.ac.nz>
2011-02-28 02:23:48 +1300

    Added a complete constructor to ImageBrowser

--- test/UnitTestImageBrowser.cpp
+++ test/UnitTestImageBrowser.cpp
@@ -8,24 +8,55 @@
 #include "../src/ImageBrowser.h"
 #include <gtest/gtest.h>
 
+class Cmiss_textre;
+
 namespace cap
 {
+
+class FakeCmguiManager
+{
+public:
+	Cmiss_texture* LoadCmissTexture(std::string const& filename) const 
+	{
+		return 0;
+	}
+};
+
 class TestImageBrowseWindow
 {
 public:
-	TestImageBrowseWindow(ImageBrowser<TestImageBrowseWindow>& browser)
+	TestImageBrowseWindow(ImageBrowser<TestImageBrowseWindow, FakeCmguiManager>& browser)
 	:
 		browser_(browser)
 	{}
+	
+	void CreateProgressDialog(std::string const& title, std::string const& msg, int max) {}
+	void UpdateProgressDialog(int count) {}
+	void DestroyProgressDialog() {}
+	
+	
 private:
-	ImageBrowser<TestImageBrowseWindow>& browser_;
+	ImageBrowser<TestImageBrowseWindow, FakeCmguiManager>& browser_;
+};
+
+class TestImageBrowseWindowClient : public ImageBrowseWindowClient
+{
+public:
+	virtual ~TestImageBrowseWindowClient() {}
+	virtual void LoadImagesFromImageBrowseWindow(SlicesWithImages const& slices)
+	{}
 };
 
 } // namespace cap
 TEST(ImageBrowser, Create)
 {
 	using namespace cap;
-	ImageBrowser<TestImageBrowseWindow> iBrowser;
+	
+	TestImageBrowseWindowClient client;
+	FakeCmguiManager cmguiManager;
+	ImageBrowser<TestImageBrowseWindow, FakeCmguiManager> iBrowser("dirname", cmguiManager, client);
 	TestImageBrowseWindow gui(iBrowser);
 	iBrowser.SetImageBrowseWindow(&gui);
+	
+	iBrowser.CreateTexturesFromDICOMFiles();
 }
