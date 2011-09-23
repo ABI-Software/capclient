f37953b8549ca79a9981fedc50d70c5fc285eb4f
Hugh Sorby <h.sorby@auckland.ac.nz>
2011-09-23 15:04:21 +1200

    Enabling ImageBrowser test.

--- test/UnitTestImageBrowser.cpp
+++ test/UnitTestImageBrowser.cpp
@@ -5,110 +5,66 @@
  *      Author: jchu014
  */
 
-#include <wx/wxprec.h>
-#include <wx/notebook.h>
-// For compilers that don't support precompilation, include "wx/wx.h";
-#ifndef WX_PRECOMP
-#include <wx/wx.h>
-#endif
-
 #include <gtest/gtest.h>
 
+#include <wx/xrc/xmlres.h>
+
 #include "imagebrowser.h"
+#include "iimagebrowserwindow.h"
 
 namespace cap
 {
 
-class FakeCmguiManager
+class FakeCAPClient : public IImageBrowserWindow
 {
 public:
-	Cmiss_texture* LoadCmissTexture(std::string const& filename) const 
-	{
-		return 0;
-	}
-};
-
-class TestImageBrowseWindow
-{
-public:
-	TestImageBrowseWindow(ImageBrowser<TestImageBrowseWindow, FakeCmguiManager>& browser,
-			FakeCmguiManager const& manager)
-		: browser_(browser)
-	{}
-	
-	void CreateProgressDialog(std::string const& title, std::string const& msg, int max) {}
-	void UpdateProgressDialog(int count) {}
-	void DestroyProgressDialog() {}
-	
-	void LoadWindowLayout() {}
-	void CreatePreviewPanel() {}
-	void FitWindow() {}
-	
-	void SetInfoField(std::string const& name, std::string const& value) {}
-	
-	void ClearImageTable() {}
-	void CreateImageTableColumns() {}
-	void PopulateImageTableRow(int rowNumber,
-			int seriesNumber, std::string const& seriesDescription,
-			std::string const& sequenceName, size_t numImages,
-			long int const& userDataPtr)
-	{}
-	
-	void SelectFirstRowInImageTable() {}
-	void SetImageTableRowLabelByUserData(long int, std::string const&)
-	{}
-	void Show(bool) {}
-	
-private:
-	ImageBrowser<TestImageBrowseWindow, FakeCmguiManager>& browser_;
-};
-
-class TestImageBrowseWindowClient : public IImageBrowseWindow
-{
-public:
-	virtual ~TestImageBrowseWindowClient() {}
-	virtual void LoadImagesFromImageBrowseWindow(SlicesWithImages const& slices, CardiacAnnotation const& anno)
+	virtual ~FakeCAPClient() {}
+	virtual void LoadImagesFromImageBrowserWindow(SlicesWithImages const& slices, CardiacAnnotation const& anno)
 	{}
 };
 
 // Link time test double
 FileSystem::FileSystem(std::string const& path)
 {}
-	
+
 std::vector<std::string> const& FileSystem::getAllFileNames()
 {
 	return filenames;
 }
 
-
 DICOMImage::DICOMImage(const std::string& filename)
 {
 }
 
+class TestApp : public wxApp
+{
+	bool OnInit()
+	{
+		wxXmlResource::Get()->InitAllHandlers();
+		return true;
+	}
+};
+
 } // namespace cap
-//TEST(ImageBrowser, Create)
-//{
-//	using namespace cap;
-//	
-//	TestImageBrowseWindowClient client;
-//	FakeCmguiManager cmguiManager;
-//	ImageBrowser<TestImageBrowseWindow, FakeCmguiManager> iBrowser("dirname", cmguiManager, client);
-//	TestImageBrowseWindow gui(iBrowser, cmguiManager);
-//	iBrowser.SetImageBrowseWindow(&gui);
-//	iBrowser.Initialize();
-//	
-//	iBrowser.CreateTexturesFromDICOMFiles();
-//}
 
 TEST(ImageBrowser, CreateUsingFactory)
 {
+	int argc = 0;
+	char **argv = 0;
 	using namespace cap;
+	FakeCAPClient client;
+	wxApp::SetInstance( new TestApp() );
+	wxEntryStart( argc, argv );
+	wxTheApp->OnInit();
 	
-	TestImageBrowseWindowClient client;
-	FakeCmguiManager cmguiManager;
+	// you can create top level-windows here or in OnInit()
+	//...
+	// do your testing here
+	ImageBrowser* ib = ImageBrowser::CreateImageBrowser("dirname", &client);
 	
-	ImageBrowser* ib = 
-			ImageBrowser::CreateImageBrowser("dirname", client);
-	
+	//wxTheApp->OnRun(); // Don't start main loop
+	wxTheApp->OnExit();
+	wxEntryCleanup();
 	delete ib;
 }
+
