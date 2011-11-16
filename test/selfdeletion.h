2af600105a2690d42cd43f8d754e06d2e2aff750
Hugh Sorby <h.sorby@auckland.ac.nz>
2011-11-16 13:36:11 +1300

    Finished working on SelfDeletion test.  The self deletion is working as expected.

---
+++ test/selfdeletion.h
@@ -0,0 +1,65 @@
+
+#ifndef SELFDELETION_H_
+#define SELFDELETION_H_
+
+#include "selfdeletionwindow.h"
+
+#include "utils/debug.h"
+
+namespace cap
+{
+	class SelfDeletion
+	{
+	public:
+		void SetSelfDeletionWindow(SelfDeletionWindow *sdw)
+		{
+			sdw_ = sdw;
+		}
+
+		void OnOK()
+		{
+			dbg("SelfDeletion::OnOK");
+			delete this;
+		}
+
+		void OnCancel()
+		{
+			dbg("SelfDeletion::OnCancel");
+			delete this;
+		}
+
+		static SelfDeletion* CreateSelfDeletion()
+		{
+			if (!wxXmlInitialised_)
+			{
+				wxXmlInitialised_ = true;
+				wxXmlInit_selfdeletionwindowui();
+			}
+
+			SelfDeletion* sd = new SelfDeletion();
+			SelfDeletionWindow* frame = new SelfDeletionWindow(sd);
+			frame->Show(true);
+			sd->SetSelfDeletionWindow(frame);
+			
+			return sd;
+		}
+
+		SelfDeletion()
+			: sdw_(0)
+		{
+		}
+
+		~SelfDeletion()
+		{
+			dbg("~SelfDeletion()");
+			if(sdw_)
+				sdw_->Destroy();
+		}
+
+		static bool wxXmlInitialised_;
+		SelfDeletionWindow *sdw_;
+	};
+}
+
+#endif /* SELFDELETION_H_ */
+
