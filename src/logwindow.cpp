3897eeb83f3a0216ed8b4c4ef7d31930b8a18486
Hugh Sorby <h.sorby@auckland.ac.nz>
2011-12-07 13:23:38 +1300

    Working on getting LogWindow working as desired.

--- src/logwindow.cpp
+++ src/logwindow.cpp
@@ -2,26 +2,93 @@
 
 #include "logwindow.h"
 
+#include "utils/filesystem.h"
 
 namespace cap
 {
 	LogWindow* LogWindow::instance_ = 0;
 
 	LogWindow::LogWindow()
+		: previousSaveLocation_("")
 	{
+		MakeConnections();
 	}
 
 	LogWindow::~LogWindow()
 	{
 	}
 
-	void MakeConnections()
+	void LogWindow::MakeConnections()
 	{
+		Connect(wxEVT_CLOSE_WINDOW, wxCloseEventHandler(LogWindow::OnCloseWindow));
+		Connect(button_exit_->GetId(), wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(LogWindow::OnExit));
+		Connect(button_save_->GetId(), wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(LogWindow::OnSave));
 	}
 
-	std::ostringstream& LogWindow::Log(LogLevelEnum level)
+	void LogWindow::OnCloseWindow(wxCloseEvent& event)
 	{
-		return oss_;
+		Hide();
 	}
+
+	void LogWindow::OnSave(wxCommandEvent& event)
+	{
+		if (previousSaveLocation_.length() == 0)
+			previousSaveLocation_ = wxGetCwd();
+
+		std::string filename = FileSystem::GetFileName(previousSaveLocation_);
+		std::string dirpath = "";//FileSystem::GetPath(previousSaveLocation_);
+
+		wxFileDialog dialog(this, "Select a file", dirpath, filename, "*.*", wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
+		dialog.SetPath(previousSaveLocation_);
+		dialog.Center();
+
+		if (dialog.ShowModal() == wxID_OK)
+		{
+			previousSaveLocation_ = dialog.GetPath();
+			std::cout << previousSaveLocation_ << std::endl;
+		}
+	}
+
+	void LogWindow::OnExit(wxCommandEvent& event)
+	{
+		Hide();
+	}
+
+	void LogWindow::LogMessage(const std::string& message)
+	{
+		html_log_->AppendToPage(message);
+	}
+
+	//std::ostringstream& Log(LogLevelEnum level)
+	//{
+	//	//Logger log;
+	//	//log.Log(level);
+	//	return //log.Log(level);
+	//}
+
+	std::string logLevelString(LogLevelEnum level)
+	{
+		static const char* const buffer[] = {"ERROR", "WARNING", "INFORMATION", "DEBUG"};
+		return buffer[level];
+	}
+
+	//Logger::Logger()
+	//{
+	//}
+	//
+	//Logger::~Logger()
+	//{
+	//	oss_ << std::endl;
+	//	LogWindow::GetInstance()->LogMessage(oss_.str());
+	//}
+
+	//std::ostringstream& Logger::Log(LogLevelEnum level)
+	//{
+	//	oss_ << TimeNow();
+	//	oss_ << " " << logLevelString(level) << ": ";
+
+	//	return oss_;
+	//}
+
 
 }
