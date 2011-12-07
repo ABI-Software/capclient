13ecac17f60ff07f8b97eff25c9719f2ceb87a5b
Hugh Sorby <h.sorby@auckland.ac.nz>
2011-12-08 10:15:52 +1300

    Splitting out logmsg from logwindow and adding a  raw message function to help with other forms of messages.  Finilising test, documentation still to do.

---
+++ src/logmsg.cpp
@@ -0,0 +1,29 @@
+
+
+#include "logmsg.h"
+
+#include "logwindow.h"
+#include "utils/debug.h"
+
+namespace cap
+{
+	LogLevelEnum Log::reportingLevel_ = LOGDEBUG;
+
+	Log::~Log()
+	{
+#ifndef CAP_CLIENT_RELEASE_BUILD
+		if (level_ > LOGINFORMATION)
+			dbgn(oss_.str());
+#endif
+		if (rawMsg_)
+			LogWindow::GetInstance()->LogMessage(oss_.str());
+		else
+		{
+			oss_ << std::endl;
+			LogWindow::GetInstance()->LogMessage(time_, level_, oss_.str());
+		}
+	}
+
+
+}
+
---
+++ src/logmsg.h
@@ -0,0 +1,82 @@
+
+
+#ifndef LOGMSG_H_
+#define LOGMSG_H_
+
+#include <iostream>
+#include <sstream>
+
+#include "utils/time.h"
+
+namespace cap
+{
+
+	enum LogLevelEnum 
+	{
+		LOGERROR,
+		LOGWARNING,
+		LOGINFORMATION,
+		LOGDEBUG
+	};
+
+	std::string logLevelString(LogLevelEnum level);
+
+	class Log
+	{
+	public:
+		Log();
+		~Log();
+		std::ostringstream& Msg(LogLevelEnum level);
+		std::ostringstream& Msg();
+		static LogLevelEnum ReportingLevel() { return reportingLevel_; }
+
+	private:
+		std::ostringstream oss_;
+		std::string time_;
+		LogLevelEnum level_;
+		bool rawMsg_;
+		static LogLevelEnum reportingLevel_;
+
+	private:
+		Log(const Log&);
+		Log& operator =(const Log&);
+	};
+
+	inline Log::Log()
+		: time_()
+		, level_(LOGINFORMATION)
+		, rawMsg_(false)
+	{
+#if defined CAP_CLIENT_RELEASE_BUILD
+		reportingLevel_ = LOGINFORMATION;
+#endif
+	}
+
+	inline std::ostringstream& Log::Msg(LogLevelEnum level)
+	{
+		time_ = TimeNow();
+		level_ = level;
+
+		return oss_;
+	}
+
+	inline std::ostringstream& Log::Msg()
+	{
+		rawMsg_ = true;
+
+		return oss_;
+	}
+
+	#define LOG_MSG(level) \
+		if (level > Log::ReportingLevel()) ; \
+		else Log().Msg(level)
+
+	#define LOG_MSG_RAW(level) \
+		if (level > Log::ReportingLevel()) ; \
+		else Log().Msg()
+
+}
+
+
+#endif /* LOGMSG_H_ */
+
