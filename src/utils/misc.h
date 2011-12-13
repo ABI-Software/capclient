c81962029db4e75c366e900dec1fddaf741248d7
hsorby <hsorby@localhost>
2011-12-13 23:09:13 +1300

    Adding string utility EndsWith and test also.

--- src/utils/misc.h
+++ src/utils/misc.h
@@ -7,7 +7,23 @@
 
 namespace cap
 {
+	/**
+	 * Cross platform function to get the current time in string format.
+	 *
+	 * @return	string formatted current time.
+	 */
 	std::string TimeNow();
+
+	/**
+	 * Ends with tests if the string str ends with the suffix suffix.
+	 *
+	 * @param	str   	The string.
+	 * @param	suffix	The suffix.
+	 *
+	 * @return	true if it succeeds, false if it fails.
+	 */
+	bool EndsWith(std::string str, std::string suffix);
+
 }
 
 #endif /* UTILS_TIME_H_ */
