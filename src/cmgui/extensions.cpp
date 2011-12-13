c5d0f8795d84970157ff8066b56ee42db7d4b2dd
Hugh Sorby <h.sorby@auckland.ac.nz>
2011-12-13 15:25:20 +1300

    Setting the label string for a created node to the name of the modelling point type.

--- src/cmgui/extensions.cpp
+++ src/cmgui/extensions.cpp
@@ -174,7 +174,7 @@ int Cmiss_context_create_region_with_nodes(Cmiss_context_id cmissContext, std::s
 				material = "pink";
 
 			//std::string node_command = "gfx modify g_element " + regionName + " node_points coordinate coordinates LOCAL glyph sphere general size \"10*10*10\" visibility visibility_control_field centre 0,0,0 font default select_on material " + material + " selected_material " + material + "_sel label " + label + ";";
-			std::string node_command = "LOCAL glyph sphere general size \"6*6*6\" visibility if_field centre 0,0,0 font node_label_font select_on material " + material + " selected_material " + material + "_selected label positive_time";// + label;
+			std::string node_command = "LOCAL glyph sphere general size \"6*6*6\" visibility if_field centre 0,0,0 font node_label_font select_on material " + material + " selected_material " + material + "_selected label " + label;
 			Cmiss_graphic_define(node_graphic, node_command.c_str());
 			Cmiss_graphic_destroy(&node_graphic);
 		}
